abe7d7305f4ea4963014079439eb41b1f3dc6236
Ali <39883081+goorekt@users.noreply.github.com>
2023-09-27 14:09:18 +0200

    menu screen animation
    spiller animation løber og gluvet rykker sig synkroniseret med hans idle animation

---
+++ GMTA/src/Screens/TileMap.gd
@@ -0,0 +1,24 @@
+extends TileMap
+
+export var speed=200
+export var index=1
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var sprite=$"../AnimatedSprite"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position.x=1000*index
+	pass # Replace with function body.
+
+func _process(delta):
+	if (position.x>1000):
+		position.x=-1100
+	if (!sprite.standing):
+		position.x+=speed*delta
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
