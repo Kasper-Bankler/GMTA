3cc77944865f8be856c83698ad3ec5ea3d2ae3e6
Christian Walker Petersen <c.walker.petersen@gmail.com>
2023-10-01 16:33:04 +0200

    Animals Fix

--- GMTA/src/Actors/wolf.gd
+++ GMTA/src/Actors/wolf.gd
@@ -1,13 +1,12 @@
-extends "res://src/Actors/animals.gd"
+extends Animals
 
 
 
 func _ready():
-	#Starter timer men bliver fjenet sene
-	$Timer.start(timerTime)
 	player = get_tree().get_nodes_in_group("player")[0]
 	assert(player!=null)
 	.add_to_group("enemies")
+	this_health=5
 
 func _physics_process(delta):
 	if this_health<1:
@@ -15,7 +14,7 @@ func _physics_process(delta):
 		isDead=true
 	if isDead:
 		return
-	
+	walk()
 	detection()
 	
 	if isChasing and is_on_floor():
@@ -24,31 +23,3 @@ func _physics_process(delta):
 		_velocity.y += 90 * delta
 	
 	move_and_slide(_velocity, FLOOR_NORMAL)
-
-
-
-func _on_Timer_timeout():
-	if isDead:
-		return
-	if goingRight:
-		dir = 1
-		anim.flip_h = false
-		Rray.cast_to = Vector2(100,0)
-		RUray.cast_to = Vector2(70,-50)
-		Lray.cast_to = Vector2(-50,0)
-		LUray.cast_to = Vector2(-45,-35)
-		_velocity.x = SPEED * dir
-		anim.play("walk")
-		goingRight = false
-		$Timer.start(timerTime)
-	elif goingRight == false:
-		dir = -1
-		_velocity.x = SPEED * dir
-		anim.flip_h = true
-		Rray.cast_to = Vector2(50,0)
-		RUray.cast_to = Vector2(45,-35)
-		Lray.cast_to = Vector2(-100,0)
-		LUray.cast_to = Vector2(-70,-50)
-		anim.play("walk")
-		goingRight = true
-		$Timer.start(timerTime)
